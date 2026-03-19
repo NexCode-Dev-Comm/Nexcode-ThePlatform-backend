8d8ffdaea856e97f00cdf4b707b773fbb05ebee0
Dovlet <ddowlet502@gmail.com>
2026-03-19 22:06:33 +0200

    Fix formatting and add newline at end of file

--- database.py
+++ database.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./nexcode.db"
+
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
@@ -17,3 +20,5 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
