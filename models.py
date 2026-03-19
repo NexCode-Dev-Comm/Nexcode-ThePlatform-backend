ea64ef7427e1f507b412b9d46628dcb7be65867a
Dovlet <ddowlet502@gmail.com>
2026-03-19 22:04:19 +0200

    Enhance ProjectModel with new fields
    Added code_name and code_content fields to ProjectModel

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Text 
 from database import Base
+
+
+
 
 class ProjectModel(Base):
     __tablename__ = "projects"
@@ -7,5 +10,11 @@ class ProjectModel(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     description = Column(String)
+    code_name = Column(String)    
+    code_content = Column(Text) 
+    
     contributors_count = Column(Integer, default=0)
     image_url = Column(String, nullable=True)
+
+
+
